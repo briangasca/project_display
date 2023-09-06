0dd299dae8ec4d365f9f4539538068cd498ec89f
briangasca <141669766+briangasca@users.noreply.github.com>
2023-09-06 06:33:55 +0000

    Added dropdown menu and layout for graphs.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
