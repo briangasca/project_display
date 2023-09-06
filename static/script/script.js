d78e02e3b10c5d1eb60322719b78349033225f3c
briangasca <141669766+briangasca@users.noreply.github.com>
2023-09-06 23:10:45 +0000

    everything here works

--- static/script/script.js
+++ static/script/script.js
@@ -5,9 +5,33 @@
 // 5: GOOGL
 
 function changeImage(){
+    let container = document.getElementById('container');
+    container.style.display = 'none';
+    container.classList.remove('fadeInDown');
+    container.classList.add('fadeIn_reload');
+    setTimeout(() => {
+        container.style.display = 'block';
+    }, 25)
+
+
+
     var choice = document.getElementById('choice').value;
     console.log(choice);
+    if (choice == "1"){
+        document.getElementById('LR').src="static/images/TSLA/lr_tsla.png";
+        document.getElementById('keras').src="static/images/TSLA/keras_tsla.png";
+    }
     if (choice == "2"){
-        document.getElementById('LR').src="static/images/keras_tsla.png"
+        document.getElementById('LR').src='';
+        document.getElementById('keras').src="static/images/APPL/keras_aapl.png";
+    }
+    if (choice == "3"){
+        document.getElementById('keras').src="static/images/META/keras_meta.png";
+    }
+    if (choice == "4"){
+        document.getElementById('keras').src="static/images/KO/keras_ko.png";
+    }
+    if (choice == "5"){
+        document.getElementById('keras').src="static/images/GOOGL/keras_googl.png";
     }
 }
