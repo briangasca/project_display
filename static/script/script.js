cf7dc25a33542093da42aafab364092cb057813b
briangasca <141669766+briangasca@users.noreply.github.com>
2023-09-07 05:23:16 +0000

    added background effects & About us page

--- static/script/script.js
+++ static/script/script.js
@@ -6,32 +6,39 @@
 
 function changeImage(){
     let container = document.getElementById('container');
+    let about_us = document.getElementById('about_us');
+
+    about_us.style.display = 'none';
     container.style.display = 'none';
     container.classList.remove('fadeInDown');
     container.classList.add('fadeIn_reload');
     setTimeout(() => {
         container.style.display = 'block';
-    }, 250)
+        about_us.style.display = 'block';
+    }, 25)
 
 
 
     var choice = document.getElementById('choice').value;
     console.log(choice);
     if (choice == "1"){
-        document.getElementById('LR').src="static/images/TSLA/lr_tsla.png";
+        document.getElementById('LR').src="static/images/TSLA/tsla_lr.png";
         document.getElementById('keras').src="static/images/TSLA/keras_tsla.png";
     }
     if (choice == "2"){
-        document.getElementById('LR').src='';
+        document.getElementById('LR').src='static/images/APPL/aapl_lr.png';
         document.getElementById('keras').src="static/images/APPL/keras_aapl.png";
     }
     if (choice == "3"){
+        document.getElementById('LR').src='static/images/META/meta_lr.png';
         document.getElementById('keras').src="static/images/META/keras_meta.png";
     }
     if (choice == "4"){
+        document.getElementById('LR').src='static/images/KO/ko_lr.png';
         document.getElementById('keras').src="static/images/KO/keras_ko.png";
     }
     if (choice == "5"){
+        document.getElementById('LR').src='static/images/GOOGL/googl_lr.png';
         document.getElementById('keras').src="static/images/GOOGL/keras_googl.png";
     }
 }
